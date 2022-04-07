9536dbe96a9500d8874c1a11f365746ae8195444
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-04-06 22:21:08 -0300

    Inclusão da resolução para o desafio 01

---
+++ codigos-desafios/desafio-01.js
@@ -0,0 +1,15 @@
+let entrada = gets().split(" ");
+
+let a = parseInt(entrada[0]);
+let b = parseInt(entrada[1]);
+
+let q = parseInt(a / b);
+
+let r = a - b * q;
+
+if (r < 0) {
+    r += Math.abs(b);
+    q = (a - r) / b;
+}
+
+console.log(parseInt(q) + " " + parseInt(r));
